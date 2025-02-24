a8ad82720c8b0420f0442b82760f185b7496c5fa
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-02-24 20:31:55 +0200

    feat: add Cloudinary image upload and optimization

--- services/imageService.js
+++ services/imageService.js
@@ -2,8 +2,6 @@ import multer from "multer";
 import path from "path";
 import { v4 } from "uuid";
 import HttpError from "../helpers/HttpError.js";
-import fse from "fs-extra";
-import sharp from "sharp";
 
 export class ImageService {
   static initUploadImageMiddleware(name) {
@@ -36,33 +34,5 @@ export class ImageService {
     }).single(name);
 
     return upload;
-  }
-
-  static async saveImage(file, size, folder) {
-    const storeImage = path.join(process.cwd(), "public", folder);
-
-    await fse.ensureDir(storeImage);
-
-    const newFilename = `${v4()}.jpeg`;
-    const outputPath = path.join(storeImage, newFilename);
-
-    try {
-      const metadata = await sharp(file.path).metadata();
-      if (metadata.width !== size.width || metadata.height !== size.height) {
-        await sharp(file.path)
-          .resize(size.width, size.height)
-          .toFormat("jpeg")
-          .jpeg({ quality: 80 })
-          .toFile(outputPath);
-      } else {
-        await sharp(file.path).toFormat("jpeg").toFile(outputPath);
-      }
-
-      await fse.remove(file.path); // Якщо необхідно видалити тимчасовий файл
-      const avatarURL = `/public/${folder}/${newFilename}`;
-      return avatarURL;
-    } catch {
-      throw HttpError(500, "Error processing image");
-    }
   }
 }
